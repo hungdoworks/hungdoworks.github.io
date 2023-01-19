6661e282301be245191f4de4e77423c61665c4d4
HungDo <hung.do@teqie.io>
2023-01-19 13:43:34 +0700

    Update Navigation Bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,19 +1,20 @@
 import styles from "@/styles/NavigationBar.module.scss";
 import Link from "next/link";
+import { HoverDropdown, HoverDropdownItem } from "./hover-dropdown";
 
 const NavigationData = [
     {title: "Home", url: "/"},
-    {title: "Performance", url: "/"},
-    {title: "Choreography", url: "/"},
+    {title: "Performance", url: "/",
+        sub: [
+            { title: <span>A <span style={{wordSpacing: "-10px"}}>Wo │ man</span></span>, url: "/" },
+            { title: "L'ego", url: "/" },
+            { title: "Cramped", url: "/" },
+        ]
+    },
+    {title: "Choreography", url: "/",},
     {title: "Workshop", url: "/"},
     {title: "Video", url: "/"},
     {title: "About", url: "/"},
-];
-
-const PerformanceData = [
-    {title: "A Wo|man", url: "/"},
-    {title: "L'ego", url: "/"},
-    {title: "Cramped", url: "/"},
 ];
 
 
@@ -23,10 +24,24 @@ export default function NavigationBar() {
             <div className={styles.navList}>
                 {
                     NavigationData.map((item, index) => {
+                        if (item.sub) {
+                            return (
+                                <HoverDropdown title={item.title}>
+                                    {
+                                        item.sub.map((subItem, subIndex) => 
+                                            <HoverDropdownItem 
+                                                key={subIndex} 
+                                                title={subItem.title}
+                                                url={subItem.url} 
+                                            />
+                                        )
+                                    }
+                                </HoverDropdown>
+                            )
+                        }
                         return <Link key={index} href={item.url}>{item.title}</Link>
                     })
                 }
-                <div style={{flexGrow: 1}}></div>
             </div>
         </div>
     );
