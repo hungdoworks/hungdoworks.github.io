b1ea49d964f6b60ada3a2b238d005d2a046e5c12
HungDo <hung.do@teqie.io>
2023-01-19 16:08:05 +0700

    Update UI for Homepage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,8 +11,8 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavigationBar />
       <main>
+        <NavigationBar />
         <Component {...pageProps} />
       </main>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import styles from "@/styles/Home.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image src="/images/bg_home.jpg" alt="" fill />
-      <div style={{ position: "absolute", color: "#FFFFFF" }}>
-        <p>Minh Hai</p>
-        <p>Dancer | Performer</p>
+      <div className={styles.containerBackground}></div>
+      <div className={styles.content}>
+        <Image src="/images/bg_home.jpg" alt="" fill />
+        <div className={styles.title}>
+          <p>Minh Hai</p>
+          <p>Dancer | Performer</p>
+        </div>
       </div>
     </div>
   )
