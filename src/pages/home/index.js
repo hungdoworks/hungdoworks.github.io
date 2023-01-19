e0a84f0457a84816ac56669ba0000083d81e511d
HungDo <hung.do@teqie.io>
2023-01-19 10:19:05 +0700

    Add home page

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+import styles from "../../styles/Home.module.scss";
+
+export default function HomePage() {
+    return (
+        <div className={styles.container}>
+            <Image src="/images/bg_home.jpg" alt="" fill/>
+            <div style={{position: "absolute", color: "#FFFFFF"}}>
+                <p>Minh Hai</p>
+                <p>Dancer | Performer</p>
+            </div>
+        </div>
+    );
+}
