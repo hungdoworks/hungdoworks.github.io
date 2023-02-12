4c06e62dbeae5f10f1180f8dea54a12c2e2b9434
HungDo <hung.do@teqie.io>
2023-02-12 15:03:59 +0700

    Add item to Performances page.

---
+++ src/data/performances/details/cramped.js
@@ -0,0 +1,40 @@
+import img0 from "../../../../public/images/performances/cramped/img_0.jpg";
+import img1 from "../../../../public/images/performances/cramped/img_1.webp";
+import img2 from "../../../../public/images/performances/cramped/img_2.webp";
+import img3 from "../../../../public/images/performances/cramped/img_3.webp";
+import img4 from "../../../../public/images/performances/cramped/img_4.webp";
+import img5 from "../../../../public/images/performances/cramped/img_5.webp";
+
+
+const title = "Chật (Cramped)";
+
+const videos = [
+    "https://www.youtube.com/embed/_OEAt9vPDEU"
+];
+
+const images = [
+    img0,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+];
+
+const description = <>
+    <span>
+        Chật – is the story on a fragile border, two individuals who have slided pass each other, met, encountered, confronted and found. The narrow and fragile space bundles tightly and compels each individual, in order to maintain their own most essential things, to scramble every bit of space. Thrusting, crushing, kicking until the end of conflict, we are dumbfounded to realize that we are empty and full of hurt. In silence, we empathize ourselves through the eyes of the opposite
+    </span>
+</>
+
+const credits = <>
+    Performed by Thanh Huong and Minh Hai.
+</>;
+
+export default {
+    title,
+    videos,
+    description,
+    credits,
+    images,
+}
--- src/data/performances/details/index.js
+++ src/data/performances/details/index.js
@@ -1,11 +1,13 @@
 import AWoman from './a-woman';
 import TamSinh from './tam-sinh';
 import Lego from './l-ego';
+import Cramped from './cramped';
 
 const PerformanceData = {
     "a-woman": AWoman,
     "tam-sinh": TamSinh,
     'l-ego': Lego,
+    'cramped': Cramped,
 };
 
 export default PerformanceData;
