ebfc1816a3a93ac7c1ccb86a6203285b41e5d41b
HungDo <hung.do@teqie.io>
2023-02-17 15:13:17 +0700

    Update credits for 'Xin Hay Nhin Em' project

--- src/data/others/details/xin-hay-nhin-em.js
+++ src/data/others/details/xin-hay-nhin-em.js
@@ -11,6 +11,49 @@ const description = <>
 </>
 
 const credits = <>
+Main Actors:
+<br />
+Minh Hải - Hương<br />
+Hương Phùng - Nhài<br />
+Nguyễn Văn Thủy - The Husband<br />
+Đặng Thuỳ Anh - Strange Girl<br />
+Ngô Hồng Ngọc - Manager<br />
+<br />
+Public Actors:
+<br />
+Ngạo Thuyên - Office Staff 1<br />
+Huyền M - Office Staff 2<br />
+Nguyễn Thị Hồng Anh - Office Staff 3<br />
+Nguyễn Thanh Tâm - Office Staff 4<br />
+Đan Vi - Office Staff 5<br />
+Nguyễn Trần Thiên Hân - Office Staff 6<br />
+Thạch Kỳ Nam - Office Staff 7<br />
+Ngân Nguyễn - Reception<br />
+<br />
+Director & Script Writer: Nguyễn Duy Anh<br />
+Assistant Director: Đào Thu Uyên<br />
+Producer: Cao Việt Nga<br />
+Assistant Director: Thạch Kỳ Nam<br />
+DOP: Thảo Hoàng<br />
+Camera Operator: Thảo Hoàng, Nguyễn Quang Vinh, Marilyn.<br />
+<br />
+Art Director: Đặng Thùy Anh<br />
+Art Construction: Nguyễn Long Biên<br />
+Costume: Vân Chi, Nguyễn Phương Kiều Anh<br />
+Make Up: Linh Tôm<br />
+Boom Operator: Phương Anh, Linh Chu, Tiến<br />
+Sound Editor: Nguyễn Duy Anh<br />
+Editor: Nguyễn Duy Anh<br />
+<br />
+DIT: Linh DN<br />
+Telesale Software Design: Cao Tuấn Minh<br />
+Filming Equipment: Bùi Kiên Trung, Metis Production<br />
+Lighting Equipment: Metis Production, Nguyễn Nhật Quang<br />
+<br />
+A great thank to:<br />
+Đinh Thảo Linh, Babau Air, ACA Space, APD Centre for Art Patronage and Development, Mrs Nguyễn Trinh Thi, Mr Nguyễn Xuân Sơn, Winni, Lucy Hymen, Nông Nhật Quang, Thái Thu Minh, Dương Hà Thu Hiền, Trần Phương Thanh, Lương Thùy Dung, Ngọc Ánh, Management Board of Ecopark, Lynhouse (Hưng Yên), Homax Office (Cầu Giấy, Hà Nội), Bùi Tuân Eyewear Store<br />
+<br />
+This film was supported by Project “Nhà Nhiều Cột”<br />
 </>;
 
 export default {
